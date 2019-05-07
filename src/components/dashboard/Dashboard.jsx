140fe98fb16974b2652f98cc4bde53e162828c5e
miguelironhack <snoop_S_@hotmail.com>
2019-05-07 15:15:02 +0200

    ports

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import UploadForm from "../upload/UploadForm";
+
+export default class Dashboard extends Component {
+  render() {
+    return <UploadForm />;
+  }
+}
