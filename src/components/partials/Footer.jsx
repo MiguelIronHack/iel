3f28271864af6f16abf38a9e583836ecd1d40575
miguelironhack <snoop_S_@hotmail.com>
2019-05-08 12:25:12 +0200

    new footer

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -8,8 +8,9 @@ export class IndexFooter extends Component {
       <Footer className="footer has-background-white-ter">
         <Container>
           <p>
-            Yield was created by Miguel Bento and Daniel Diaz <br /> &copy; 2019
-            The source code is licensed MIT
+            iel was created by Miguel Bento and Daniel Diaz, Frank did the
+            Backend so nobody sees the shit he does. Gerard is making a bot{" "}
+            <br /> &copy; 2019 The source code is licensed MIT
           </p>
           <NextSection className="footer-btn next-section-btn" href="#" />
         </Container>
