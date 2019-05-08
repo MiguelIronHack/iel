fbefb77fe4a09eb352d66eccb9bcf645db879292
miguelironhack <snoop_S_@hotmail.com>
2019-05-08 15:05:40 +0200

    course page structure

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { Progress, Heading } from "react-bulma-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faComments, faBook } from "@fortawesome/free-solid-svg-icons";
-
+import {
+  faComments,
+  faBook,
+  faCircle,
+  faCheckCircle
+} from "@fortawesome/free-solid-svg-icons";
+import CourseTable from "./partials/CourseTable";
 import "./course.css";
 
 export default class Courses extends Component {
@@ -10,68 +15,59 @@ export default class Courses extends Component {
     return (
       <section className="course-section columns is-mobile">
         <aside className="panel course-panel column">
-          <p className="panel-heading">Sidebar</p>
+          <p className="panel-heading" />
 
-          <label className="panel-block">
+          <label className="panel-block has-background-dark has-text-white aside-panel-text">
             Week 1
-            <input className="aside-panel-margin" type="checkbox" />
-          </label>
-          <label className="panel-block">
-            Week 2
-            <input className="aside-panel-margin" type="checkbox" />
-          </label>
-          <label className="panel-block">
-            Week 3
-            <input className="aside-panel-margin" type="checkbox" />
-          </label>
-
-          <div className="panel-block is-active">
-            <div>Forum</div>
-            <span className="panel-icon aside-panel-margin">
-              <FontAwesomeIcon icon={faBook} />
+            <span className="panel-icon aside-panel-icon">
+              <FontAwesomeIcon icon={faCircle} />
             </span>
-          </div>
+          </label>
+          <label className="panel-block has-background-dark has-text-white aside-panel-text">
+            Week 2
+            <span className="panel-icon aside-panel-icon">
+              <FontAwesomeIcon icon={faCircle} />
+            </span>
+          </label>
+          <label className="panel-block has-background-dark has-text-white aside-panel-text">
+            Week 3
+            <span className="panel-icon aside-panel-icon">
+              <FontAwesomeIcon icon={faCircle} />
+            </span>
+          </label>
 
-          <div className="panel-block">
-            <div className="">Course Info</div>
-            <span className="panel-icon aside-panel-margin">
+          <div className="panel-block is-active has-background-dark has-text-white">
+            <div className="aside-panel-text">Forum</div>
+            <span className="panel-icon aside-panel-icon">
               <FontAwesomeIcon icon={faComments} />
             </span>
           </div>
+
+          <div className="panel-block has-background-dark has-text-white">
+            <div className="aside-panel-text"> Info</div>
+            <span className="panel-icon aside-panel-icon">
+              <FontAwesomeIcon icon={faBook} />
+            </span>
+          </div>
+          <p className="panel-heading" />
         </aside>
         <div
-          className="column is-four-fifths
+          className="course-content column is-four-fifths
 "
         >
-          <Progress max={100} value={25} color="success" />
+          <Heading size={2}>Course Title</Heading>
+          <Progress max={100} value={25} color="success" id="progress" />
           <div className="box">
-            <Heading size={4}>Course Title</Heading>
-            <div className="columns">
-              <article className="message is-dark column">
-                <div className="message-body">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
-                  <strong>Pellentesque risus mi</strong>, tempus quis placerat
-                  ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
-                  fringilla. Nullam gravida purus diam, et dictum efficitur.
-                  Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec
-                  sodales, arcu et sollicitudin porttitor, tortor urna tempor
-                  ligula, id porttitor mi magna a neque. Donec dui urna,
-                  vehicula et sem eget, facilisis sodales sem.
-                </div>
-              </article>
-              <article className="message is-dark column">
-                <div className="message-body">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
-                  <strong>Pellentesque risus mi</strong>, tempus quis placerat
-                  ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
-                  fringilla. Nullam gravida purus diam, et dictum efficitur.
-                  Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec
-                  sodales, arcu et sollicitudin porttitor, tortor urna tempor
-                  ligula, id porttitor mi magna a neque. Donec dui urna,
-                  vehicula et sem eget, facilisis sodales sem.
-                </div>
-              </article>
-            </div>
+            <Heading size={4}>Week 1</Heading>
+            <CourseTable />
+          </div>
+          <div className="box">
+            <Heading size={4}>Week 2</Heading>
+            <CourseTable />
+          </div>
+          <div className="box">
+            <Heading size={4}>Week 3</Heading>
+            <CourseTable />
           </div>
         </div>
       </section>
