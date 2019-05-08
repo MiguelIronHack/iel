03b84a3272ee78d171529c894bff91959e1958aa
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-05-08 19:26:00 +0200

    basic style done course info page

--- src/pages/courseInfo/CourseInfo.jsx
+++ src/pages/courseInfo/CourseInfo.jsx
@@ -1,33 +1,49 @@
 import React from "react";
-import { Heading } from "react-bulma-components";
+import { Heading, Columns } from "react-bulma-components";
 import Btn from "../../components/partials/Btn";
 import "./courseInfo.css";
 
 export default function courseInfo() {
   return (
     <section className="course-info">
-      <Heading className="has-text-centered	">
-        <p className="category">Category: What a great category</p>
-        <p className="course-title">COURSE TITLE</p>
-        <p className="contributor">Contributor</p>
-        <p className="rate">Rate: *****</p>
-
-        <Btn className="enroll-btn" name="Enroll Now!" />
+      <Heading size={4} className="	category">
+        Category: What a great category
       </Heading>
-      <nav class="navbar-brand">
-        <ul class="navbar-start">
-          <li class="nav">
-            <a class="navbar-item" href="#about">
+      <Heading size={2} className="	category">
+        COURSE TITLE
+      </Heading>
+      <Heading size={4} className="	category">
+        Contributor
+      </Heading>
+      <Heading size={5} className="	category">
+        Rate: *****
+      </Heading>
+
+      <Btn className="enroll-btn" name="Enroll Now!" />
+
+      <nav className="nav has-background-dark ">
+        <ul className="navbar-start ">
+          <li>
+            <a
+              className="navbar-item has-text-white-ter course-info-nav-link"
+              href="#about"
+            >
               About
             </a>
           </li>
-          <li class="nav">
-            <a class="navbar-item" href="#program">
+          <li>
+            <a
+              className="navbar-item has-text-white-ter course-info-nav-link"
+              href="#program"
+            >
               Program
             </a>
           </li>
-          <li class="nav">
-            <a class="navbar-item" href="#contributor">
+          <li>
+            <a
+              className="navbar-item has-text-white-ter course-info-nav-link"
+              href="#contributor"
+            >
               Contributor
             </a>
           </li>
@@ -36,26 +52,6 @@ export default function courseInfo() {
       <article id="about">
         <h1 className="headers">About</h1>
         <p className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-          incidunt, molestias aliquid excepturi eos cumque ullam, voluptatem
-          architecto ab cum sequi, voluptatibus ipsum soluta! Inventore
-          voluptatibus quo facere iste asperiores veniam dolorum dignissimos
-          natus officiis. Quam molestias temporibus vel in dolores perferendis
-          odit et impedit assumenda maiores! Facere, assumenda accusamus. Ipsum,
-          quia provident? Autem sequi earum distinctio aut magnam ducimus
-          aliquid reiciendis nobis temporibus adipisci quia fugiat vitae quis
-          asperiores commodi saepe harum odio et ipsam, error sunt, ratione
-          esse, incidunt eligendi? Assumenda perspiciatis fugiat eveniet sunt
-          amet. Consequuntur omnis, ducimus voluptas impedit enim recusandae,
-          exercitationem explicabo amet a eaque labore odit asperiores ullam
-          facere, quas temporibus mollitia expedita quis ex eius excepturi
-          molestias modi incidunt. Eligendi blanditiis, sequi perspiciatis nisi
-          soluta officia temporibus inventore. Quasi, dolorum harum! Quia
-          nostrum ab, minima id atque deleniti voluptatem facilis debitis soluta
-          voluptate omnis temporibus nisi assumenda alias officiis excepturi
-          tempora aut reiciendis! Qui doloribus aliquam reiciendis a quasi,
-          placeat sit, ab quae assumenda consequuntur eveniet enim facilis
-          debitis nihil molestias corrupti esse atque laboriosam dolorum ad.
           Quod impedit provident esse harum itaque deserunt exercitationem
           fugiat vero, ut ducimus sit ab magni quidem, commodi obcaecati ratione
           nesciunt veniam, maxime fuga mollitia qui vel optio? Debitis eligendi
@@ -69,9 +65,6 @@ export default function courseInfo() {
             <h2 className="module-title">Module 1</h2>
             <p className="description">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
-              dignissimos consequuntur maiores voluptatum tempora odit illo
-              blanditiis pariatur amet, animi autem totam in accusamus. Tenetur
-              voluptate nesciunt mollitia distinctio praesentium.
             </p>
             <ul className="lessons">
               <li className="single-lesson">Lesson1</li>
@@ -127,37 +120,43 @@ export default function courseInfo() {
           </div>
         </div>
       </article>
-      <article className="card-container">
-        <div className="card opinnion">
-          <div className="card-content">
-            <p className="title">
-              “There are two hard things in computer science: cache
-              invalidation, naming things, and off-by-one errors.”
-            </p>
-            <p className="subtitle">Jeff Atwood</p>
-          </div>
+      <article className="opinions card-container">
+        <div className="opinnion has-background-white">
+          <p>
+            “There are two hard things in computer science: cache invalidation,
+            naming things, and off-by-one errors.”
+          </p>
+          <p className="subtitle">Jeff Atwood</p>
         </div>
-        <div className="card opinnion">
-          <div className="card-content">
-            <p className="title">
-              “There are two hard things in computer science: cache
-              invalidation, naming things, and off-by-one errors.”
-            </p>
-            <p className="subtitle">Jeff Atwood</p>
-          </div>
+        <div className="opinnion has-background-white">
+          <p>
+            “There are two hard things in computer science: cache invalidation,
+            naming things, and off-by-one errors.”
+          </p>
+          <p className="subtitle">Jeff Atwood</p>
         </div>
-        <div className="card opinnion">
-          <div className="card-content">
-            <p className="title">
-              “There are two hard things in computer science: cache
-              invalidation, naming things, and off-by-one errors.”
-            </p>
-            <p className="subtitle">Jeff Atwood</p>
-          </div>
+        <div className="opinnion has-background-white">
+          <p>
+            “There are two hard things in computer science: cache invalidation,
+            naming things, and off-by-one errors.”
+          </p>
+          <p className="subtitle">Jeff Atwood</p>
         </div>
       </article>
       <article id="contributor">
         <h1 className="headers">Contributor</h1>
+        <div className="contributor-content">
+          <img
+            src="https://media.licdn.com/dms/image/C5603AQGcTUgKHAB77A/profile-displayphoto-shrink_100_100/0?e=1562803200&v=beta&t=ZY_n3oeeuttVY2l7YpkB9m77ufSZ_2Eqx1ro3FLoeGc"
+            alt="contributor-image"
+          />
+          <p className="contributor-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+            reiciendis maiores neque dicta iste fugiat assumenda nihil,
+            inventore accusamus ipsam dolorum iusto culpa quae doloremque vitae
+            nobis mollitia veritatis dolor?
+          </p>
+        </div>
       </article>
     </section>
   );
