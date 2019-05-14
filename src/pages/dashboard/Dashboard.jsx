a9179ae5b254f03dea91c2330474e3640b727bc3
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-05-14 14:44:30 +0200

    Style in dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import DashboardNav from "./components/DashboardNav";
+import "./components/dashboardComponents.css";
+import { Link } from "react-router-dom";
 
 export default class Dashboard extends Component {
   render() {
     return (
       <React.Fragment>
         <DashboardNav />
+        <div className="dashboard-icons">
+          <Link to="/userManagement">
+            <i class="fas fa-users fa-9x">
+              <span>Users</span>
+            </i>
+          </Link>
+          <Link to="/userManagement">
+            <i class="fab fa-leanpub fa-9x">
+              <span>Courses</span>
+            </i>
+          </Link>
+          <Link to="/userManagement">
+            <i class="fas fa-object-group fa-9x">
+              <span>Categories</span>
+            </i>
+          </Link>
+        </div>
       </React.Fragment>
     );
   }
