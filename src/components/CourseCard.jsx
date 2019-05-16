f108038c496a1b421c3d3f438051d20599146ee2
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-05-16 10:38:02 +0200

    l

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -12,30 +12,34 @@ export class Course extends Component {
   };
 
   componentDidMount() {
-    const user = getLocalToken();
-    getUser(user._id)
-      .then(res => {
-        this.setState({
-          user: res.data
-        });
-        // console.log(this.state.user, " user before enroll");
-      })
-      .catch(err => console.error(err.response, "qqqqqq"));
-    // console.log(user, " eyeyeye this is the user");
+    if (getLocalToken()) {
+      const user = getLocalToken();
+      getUser(user._id)
+        .then(res => {
+          this.setState({
+            user: res.data
+          });
+          // console.log(this.state.user, " user before enroll");
+        })
+        .catch(err => console.error(err.response, "qqqqqq"));
+      console.log(user, " eyeyeye this is the user");
+    }
   }
 
   handleClick = e => {
-    const courseId = e.id;
-    console.log(courseId, " course IDIDIDI");
+    if (getLocalToken()) {
+      const courseId = e.id;
+      console.log(courseId, " course IDIDIDI");
 
-    this.state.user.enrolledCourses.push(courseId);
-    console.log(this.state.user, " user after enroll");
-    const { enrolledCourses, _id } = this.state.user;
-    editUser(_id, { enrolledCourses })
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => console.log(err));
+      this.state.user.enrolledCourses.push(courseId);
+      console.log(this.state.user, " user after enroll");
+      const { enrolledCourses, _id } = this.state.user;
+      editUser(_id, { enrolledCourses })
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => console.log(err));
+    }
   };
 
   render() {
