a86b020a2582b93d91e557824ad2fc869fc6d61f
1testsample <frankomarmier@gmail.com>
2019-05-16 02:58:18 +0200

    dzada

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Dropdown } from "react-bulma-components";
 import { logout } from "../api/userHandler";
+import Dashboard from "./../pages/dashboard/Dashboard";
 
 export default class NavBarDropdown extends React.Component {
   state = {
@@ -38,26 +39,36 @@ export default class NavBarDropdown extends React.Component {
 
   render() {
     const { isAuth } = this.state;
+    if (isAuth)
+      return (
+        <Dropdown
+          className="navmain-dropdown columns is-vcentered has-margin-left-4 is-hoverable"
+          value={this.state.selected}
+          onChange={this.onChange}
+        >
+          <Dropdown.Item value="username">
+            <div onClick={() => console.log("user")}>Username</div>
+          </Dropdown.Item>
+          <Dropdown.Item value="settings">
+            <div onClick={this.handleClick}>Settings</div>
+          </Dropdown.Item>
+          <Dropdown.Item value="sign-out">
+            <div onClick={this.handleClick}>Sign Out</div>
+          </Dropdown.Item>
+        </Dropdown>
+      );
+
     return (
       <Dropdown
         className="navmain-dropdown columns is-vcentered has-margin-left-4 is-hoverable"
         value={this.state.selected}
         onChange={this.onChange}
       >
-        <Dropdown.Item value="username">
-          <div onClick={this.handleClick}>Username</div>
-        </Dropdown.Item>
         <Dropdown.Item value="sign-up">
           <div onClick={this.handleClick}>Sign Up</div>
         </Dropdown.Item>
-        <Dropdown.Item value="settings">
-          <div onClick={this.handleClick}>Settings</div>
-        </Dropdown.Item>
         <Dropdown.Item value="register">
           <div onClick={this.handleClick}>Register</div>
-        </Dropdown.Item>
-        <Dropdown.Item value="sign-out">
-          <div onClick={this.handleClick}>Sign Out</div>
         </Dropdown.Item>
       </Dropdown>
     );
