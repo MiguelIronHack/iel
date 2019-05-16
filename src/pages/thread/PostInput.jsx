9a733af538c08c6ba63badaa500bc9e81884dea9
lamahxx <frankonthegrill@gmail.com>
2019-05-17 00:22:43 +0200

    dzada

--- src/pages/thread/PostInput.jsx
+++ src/pages/thread/PostInput.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-const PostInput = () => {
+const PostInput = ({ value, name, handleKeyDown, handleChange }) => {
   return (
     <div className="control  has-icons-right">
       <input
+        onKeyDown={e => handleKeyDown(e)}
+        onChange={e => handleChange(e)}
+        name={name}
         className="input"
         type="text"
+        value={value}
         placeholder="Ask any questions related to the course here !"
       />
       <span className="icon is-small is-right">
-        <i class="fas fa-paper-plane" />
+        <i className="fas fa-paper-plane" />
       </span>
     </div>
   );
