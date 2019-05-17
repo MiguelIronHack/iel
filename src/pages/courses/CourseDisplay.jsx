8aa0abb9809b8b45ce5243092a43cfd91e237f49
miguelironhack <snoop_S_@hotmail.com>
2019-05-17 14:06:51 +0200

    hi

--- src/pages/courses/CourseDisplay.jsx
+++ src/pages/courses/CourseDisplay.jsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import { Progress, Heading } from "react-bulma-components";
 import CourseTable from "../components/CourseTable";
 import CourseSidePanel from "../components/CourseSidePanel";
+import { Link } from "react-router-dom";
 import { getCourse } from "../../api/coursesHandler";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
+
 import "./course.css";
 
 export default class Courses extends Component {
@@ -22,19 +26,39 @@ export default class Courses extends Component {
 
   render() {
     const { courseModules } = this.state.course;
+    console.log(courseModules);
     if (!courseModules) return null;
     return (
-      <section className="course-section columns is-mobile">
-        <div className="course-content column is-four-fifths">
-          <Heading size={2}>{this.state.course.title}</Heading>
-          <Progress max={100} value={25} color="success" id="progress" />
+      <>
+        <aside className="panel course-panel column shadow">
+          <p className="panel-heading" />
+
           {courseModules.map((mod, i) => (
-            <div key={i} className="box course-box">
-              <CourseTable mod={mod} index={i} />
-            </div>
+            <a href={`#${mod._id}`}>
+              <label className="panel-block has-background-dark has-text-white aside-panel-text">
+                {`Module ${i + 1}`}
+                <span className="panel-icon aside-panel-icon">
+                  <FontAwesomeIcon icon={faAddressCard} />
+                </span>
+              </label>
+            </a>
           ))}
-        </div>
-      </section>
+
+          <p className="panel-heading panel-footer" />
+        </aside>
+
+        <section className="course-section columns is-mobile">
+          <div className="course-content column is-four-fifths">
+            <Heading size={2}>{this.state.course.title}</Heading>
+            <Progress max={100} value={25} color="success" id="progress" />
+            {courseModules.map((mod, i) => (
+              <div key={i} className="box course-box">
+                <CourseTable mod={mod} index={i} />
+              </div>
+            ))}
+          </div>
+        </section>
+      </>
     );
   }
 }
