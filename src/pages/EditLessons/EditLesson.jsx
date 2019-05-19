82b00d132ba5336b0831ff400e7d68f761e079d8
1testsample <frankomarmier@gmail.com>
2019-05-19 15:49:38 +0200

    dzada

--- src/pages/EditLessons/EditLesson.jsx
+++ src/pages/EditLessons/EditLesson.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
-import { getOneLesson } from "../../api/lessonHandler";
-import { convertFromRaw } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState } from "draft-js";
+import { convertToRaw, convertFromRaw } from "draft-js";
+import "../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "../createLesson/createLesson.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Dropdown from "../../components/RealDropDown";
+import { getAllTags } from "../../api/tagHandler";
+import SidePanel from "../components/CourseSidePanel";
+import { faPlus, faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import { getOneLesson } from "./../../api/lessonHandler";
 class EditLesson extends Component {
   state = {};
 
   componentDidMount() {
+    getAllTags()
+      .then(({ data }) => {
+        this.setState({
+          tags: data,
+          selectedTag: data[0]
+        });
+      })
+      .catch(err => console.log("There was an error with the db", err));
+
     const lessonId = "5cdcd608fec7a30cccb40b44";
     getOneLesson(lessonId)
       .then(({ data }) => {
         const x = data.content;
         const converted = convertFromRaw(JSON.parse(x));
-        console.log(converted);
+        const editorState = EditorState.createWithContent(converted);
+        this.setState({ editorState: editorState });
       })
       .catch(err => console.log(err));
   }
@@ -18,10 +38,19 @@ class EditLesson extends Component {
   handleInput = e => {};
   handleSubmit = e => {};
 
+  onContentStateChange = editorState => {
+    this.setState({ editorState });
+  };
+
+  saveChanges = e => {
+    console.log("i have been clicked");
+  };
+
   render() {
+    const { selectedTag, tags } = this.state;
     return (
       <>
-        {/* <div className="form-submit-lesson">
+        <div className="form-submit-lesson">
           <form onSubmit={this.handleSubmit}>
             <div className="column is-4">
               <input
@@ -29,7 +58,7 @@ class EditLesson extends Component {
                 placeholder="Lesson title"
                 name="title"
                 onChange={this.handleInput}
-                value={title}
+                // value={title}
               />
             </div>
             <div className="column is-4">
@@ -38,7 +67,7 @@ class EditLesson extends Component {
                 placeholder="Description"
                 name="description"
                 onChange={this.handleInput}
-                value={description}
+                // value={description}
               />
             </div>
             <div className="dropdownNsubmit">
@@ -71,7 +100,7 @@ class EditLesson extends Component {
 
             <ToastContainer />
           </form>
-        </div> */}
+        </div>
       </>
     );
   }
