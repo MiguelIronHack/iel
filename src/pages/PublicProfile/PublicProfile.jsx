b24eee98384e5d3b1b50f708e484428920d6f3dc
1testsample <frankomarmier@gmail.com>
2019-05-20 12:42:15 +0200

    dzada

--- src/pages/PublicProfile/PublicProfile.jsx
+++ src/pages/PublicProfile/PublicProfile.jsx
@@ -4,20 +4,44 @@ import UserPublicCard from "./../../components/UserPublicCard";
 import TagChart from "../profile/TagChart";
 import List from "./../../components/List";
 import { CourseCard } from "./../../components/CourseCard";
+import { getUserCourses } from "../../api/coursesHandler";
 class PublicProfile extends Component {
   state = {};
 
   componentDidMount() {
+    const userId = this.props.match.params.id;
+
     const navigation = [
       { path: "enrolledCourses", name: "Enrolled courses" },
       { path: "finishedCourses", name: "Finished courses" }
     ];
 
-    const userId = this.props.match.params.id;
     getUserPublic(userId)
       .then(({ data: user }) =>
         this.setState({ user, navigation, selectedPath: navigation[0] })
       )
+      .catch(err => console.log(err));
+
+    getUserCourses(userId)
+      .then(({ data: courses }) => {
+        const coursePopularityPerCategory = {};
+        courses.forEach(course => {
+          console.log(course);
+          if (
+            coursePopularityPerCategory.hasOwnProperty(course.category[0].name)
+          ) {
+            coursePopularityPerCategory[course.category[0].name] =
+              coursePopularityPerCategory[course.category[0].name] +
+              course.likes.length;
+          } else if (
+            !coursePopularityPerCategory.hasOwnProperty(course.category.name)
+          ) {
+            coursePopularityPerCategory[course.category[0].name] =
+              course.likes.length;
+          }
+        });
+        this.setState({ coursePopularityPerCategory });
+      })
       .catch(err => console.log(err));
   }
 
@@ -29,7 +53,7 @@ class PublicProfile extends Component {
     const { navigation, user, selectedPath } = this.state;
     if (!user || !navigation) return <h1>Error</h1>;
     const selectedView = user[selectedPath.path];
-
+    console.log(this.state.coursePopularityPerCategory);
     return (
       <section className="section">
         <div className="columns is-12">
